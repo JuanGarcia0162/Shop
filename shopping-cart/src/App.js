bd5d3de1e7c0b4eb1162b2fa2b26f022e3576d47
JuanGarcia0162 <juand.garciac2@uqvirtual.edu.co>
2021-11-23 23:11:34 -0500

    Se agregaron 4 componentes (Button,Layout,Producto,Productos) e imagenes

---
+++ shopping-cart/src/App.js
@@ -0,0 +1,32 @@
+import { Component } from 'react'
+import Productos from './components/Productos'
+import Layout from './components/Layout'
+
+class App extends Component {
+  state = {
+    productos: [
+      {name: 'Netflix', price: 10000, img: '/productos/1.png'},
+      {name: 'Disney', price: 6000, img: '/productos/2.png'},
+      {name: 'Spotify', price: 6000, img: '/productos/3.png'},
+      /* {name: 'HBO', price: 6000, img: '/productos/4.png'},
+      {name: 'Youtube', price: 6000, img: '/productos/5.png'},
+      {name: 'Amazon', price: 6000, img: '/productos/6.png'},
+      {name: 'Deezer', price: 10000, img: '/productos/7.png'},
+      {name: 'Crunchyroll', price: 6000, img: '/productos/8.png'}, */
+    ]
+  }
+  render() {
+    return (
+      <div>
+        <Layout>
+        <Productos
+          agregarAlCarro={()=> console.log('Agregar al carro')}
+          productos={this.state.productos}
+        />
+        </Layout>
+      </div>
+    )
+  }
+}
+
+export default App;
